b079bedcd4013de7ccfa71fcf6db327b3929b8ab
Diogo Perin <diogopers@gmail.com>
2017-03-23 21:31:15 -0300

    seed improvement

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,18 @@ genders = %w(macho fêmea)
 castrated = [true, false]
 breeds = YAML.load_file('config/breeds.yml')
 
+puts "Creating Users"
 800.times {
   User.create( fullname: Faker::Name.name,
                phone: Faker::PhoneNumber.cell_phone )
 }
+puts "Users created"
 
+puts "Creating Pets"
 1000.times {
   Pet.create( fullname: Faker::Pokemon.name, breed: breeds.sample,
               gender: genders.sample, castrated: castrated.sample,
               birth_date: Faker::Date.birthday(0, 25),
               last_visit: Faker::Date.backward(730), user: User.all.sample )
             }
+puts "Pets created"
